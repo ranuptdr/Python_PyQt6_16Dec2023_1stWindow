80137e653a5b25c9a6ea814fb5d20d2cda1a558a
ranuptdr <ranupatidar4589@gmail.com>
2023-12-16 21:14:51 +0530

    new

---
+++ firstwindow.py
@@ -0,0 +1,12 @@
+import sys # sys is a built-in module in python
+# from top-level module.submodul import  element1,element2,........
+from PyQt6.QtWidgets import QApplication, QWidget
+
+# classobject = ClassName()
+# Creating an instance/object of QApplication
+ranu = QApplication([]) # []passing am empty list, ranu is a external clas object
+win = QWidget()  # win  is a external clas object,   Creating an instance of QWidget
+# classobject.method()
+win.show() 
+ranu.exec()
+
